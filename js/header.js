a1fb59de537b750fbc68f5e8b8ed1f18102f708e
bobnbc <robert.liu@njcourts.gov>
2024-02-15 11:30:49 -0500

    initial commit

---
+++ js/header.js
@@ -0,0 +1,38 @@
+let menuItems = [
+    'Home',
+    'Porfolio',
+    'Awards',
+    'About',
+    'Contact'
+];
+
+let urls = [
+    "index.html",
+    "samples.html",
+    "https://www.emmys.com/",
+    "about.php",
+    "contact.php"
+];
+
+window.onload = function() {
+    /*menu elements and loop*/
+    let index = 0;
+    let menuElement = document.getElementById('menu');
+    menuElement.style.border = 'none';
+
+    let ulElement = document.querySelector('ul');
+    ulElement.classList.add('horizontal-menu'); //found in styles.css
+
+    menuItems.forEach(function(text, index) {
+            let li = document.createElement('li');
+            li.innerHTML = text;
+
+            let linkElement = document.createElement('a');
+            linkElement.href = urls[index];
+            linkElement.textContent = menuItems[index];
+            linkElement.classList.add('link'+index);
+
+            li.setAttribute('a', urls[index]);
+            ulElement.appendChild(linkElement);
+    });
+}
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let ulElement = document.querySelector('ul');
+let target = ulElement.getAttribute('data-menuTarget') || '';
+
+let tablet = document.querySelector('table');
+tablet.classList.add('classic');
